d8d92aab8183481b45dd4b54a42a3a099c2cc5f7
sakaethossain <sakaethossain122@gmail.com>
2022-02-26 16:41:29 +0600

    egwe

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// /* JS */
+// $(function () {
+//   //    sticky navbar
+//   $(window).scroll(function () {
+//     var scrolling = $(this).scrollTop();
+//     var sticky = $(".navbar");
+//     if (scrolling >= 20) {
+//       sticky.addClass("navbg");
+//     } else {
+//       sticky.removeClass("navbg");
+//     }
+//   });
+// });
